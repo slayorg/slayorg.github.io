e795dd115ae5603f988d648f81e4f741c2929c30
slayorg <36648867+slayorg@users.noreply.github.com>
2025-01-02 18:03:03 -0500

    Added palworld calculator

---
+++ palworld/assets/index-CZHiGjvm.js
@@ -0,0 +1,246 @@
+var W=Object.defineProperty;var j=(n,t,e)=>t in n?W(n,t,{enumerable:!0,configurable:!0,writable:!0,value:e}):n[t]=e;var P=(n,t,e)=>j(n,typeof t!="symbol"?t+"":t,e);(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const i of document.querySelectorAll('link[rel="modulepreload"]'))l(i);new MutationObserver(i=>{for(const s of i)if(s.type==="childList")for(const c of s.addedNodes)c.tagName==="LINK"&&c.rel==="modulepreload"&&l(c)}).observe(document,{childList:!0,subtree:!0});function e(i){const s={};return i.integrity&&(s.integrity=i.integrity),i.referrerPolicy&&(s.referrerPolicy=i.referrerPolicy),i.crossOrigin==="use-credentials"?s.credentials="include":i.crossOrigin==="anonymous"?s.credentials="omit":s.credentials="same-origin",s}function l(i){if(i.ep)return;i.ep=!0;const s=e(i);fetch(i.href,s)}})();const H=`Chikipi	1500	66\r
+Teafant	1490	17\r
+Mau	1480	7\r
+Lamball	1470	31\r
+Cattiva	1460	50\r
+Cremis	1455	147\r
+Vixy	1450	84\r
+Mau Cryst	1440	8\r
+Lifmunk	1430	10\r
+Hangyu Cryst	1422	36\r
+Hangyu	1420	35\r
+Sparkit	1410	69\r
+Flambelle	1405	149\r
+Foxparks	1400	24\r
+Hoocrates	1390	88\r
+Depresso	1380	51\r
+Jolthog	1370	21\r
+Jolthog Cryst	1360	22\r
+Pengullet	1350	27\r
+Tocotoco	1340	11\r
+Fuack	1330	63\r
+Bristla	1320	25\r
+Ribbuny	1310	123\r
+Foxparks Cryst	1305	182\r
+Swee	1300	119\r
+Killamari	1290	91\r
+Flopie	1280	97\r
+Kelpsea Ignis	1270	90\r
+Kelpsea	1260	89\r
+Tanzee	1250	113\r
+Smokie	1245	172\r
+Gumoss	1240	118\r
+Daedream	1230	112\r
+Fuddler	1220	107\r
+Dazzi	1210	28\r
+Ribbuny Botan	1205	184\r
+Mimog	1200	165\r
+Woolipop	1190	43\r
+Nox	1180	128\r
+Wixen	1160	85\r
+Rooby	1155	150\r
+Maraith	1150	55\r
+Wixen Noct	1150	86\r
+Leezpunk Ignis	1140	62\r
+Rushoar	1130	9\r
+Kikit	1125	158\r
+Leezpunk	1120	61\r
+Dazzi Noct	1115	187\r
+Lunaris	1110	26\r
+Gobfin Ignis	1100	30\r
+Gobfin	1090	29\r
+Cawgnito	1080	48\r
+Beegarde	1070	101\r
+Direhowl	1060	18\r
+Vaelet	1050	95\r
+Gorirat	1040	19\r
+Galeclaw	1030	15\r
+Gorirat Terra	1030	20\r
+Robinquill	1020	56\r
+Felbat	1010	74\r
+Robinquill Terra	1000	57\r
+Verdash	990	109\r
+Fenglope	980	52\r
+Loupmoon	950	34\r
+Yakumo	945	162\r
+Lovander	940	87\r
+Caprity	930	80\r
+Eikthyrdeer	920	12\r
+Mozzarina	910	92\r
+Lullu	905	155\r
+Eikthyrdeer Terra	900	13\r
+Dumud	895	148\r
+Melpaca	890	45\r
+Reindrix	880	81\r
+Celaray	870	139\r
+Broncherry	860	75\r
+Caprity Noct	855	183\r
+Digtoise	850	14\r
+Broncherry Aqua	840	76\r
+Fenglope Lux	835	189\r
+Kitsun	830	60\r
+Tarantriss	825	176\r
+Dinossom	820	67\r
+Celesdir	815	173\r
+Dinossom Lux	810	68\r
+Loupmoon Cryst	805	185\r
+Chillet	800	130\r
+Croajiro	795	154\r
+Arsox	790	105\r
+Chillet Ignis	790	131\r
+Petallia	780	141\r
+Foxcicle	760	110\r
+Prunelia	755	2\r
+Tombat	750	102\r
+Rayhound	740	106\r
+Kitsun Noct	735	186\r
+Shroomer Noct	730	157\r
+Splatterina	725	175\r
+Shroomer	720	156\r
+Blazehowl	710	114\r
+Nitemary	705	169\r
+Katress	700	121\r
+Katress Ignis	690	122\r
+Univolt	680	23\r
+Dazemu	675	164\r
+Blazehowl Noct	670	115\r
+Dogen	665	163\r
+Vanwyrm	660	64\r
+Bushi Noct	650	138\r
+Nyafia	645	1\r
+Bushi	640	137\r
+Omascul	630	174\r
+Vanwyrm Cryst	620	65\r
+Incineram	590	5\r
+Incineram Noct	580	6\r
+Anubis	570	4\r
+Surfent	560	46\r
+Surfent Terra	550	47\r
+Sootseer	545	159\r
+Elphidran	540	41\r
+Elphidran Aqua	530	42\r
+Penking	520	129\r
+Grintale	510	142\r
+Gildane	505	3\r
+Azurobe	500	49\r
+Cinnamoth	490	143\r
+Wumpo Botan	480	94\r
+Kingpaca	470	116\r
+Xenovader	465	166\r
+Wumpo	460	93\r
+Sibelyx	450	83\r
+Kingpaca Cryst	440	117\r
+Xenogard	435	167\r
+Mossanda	430	103\r
+Nitewing	420	96\r
+Sweepa	410	120\r
+Azurmane	400	177\r
+Mossanda Lux	390	104\r
+Ragnahawk	380	136\r
+Faleris	370	77\r
+Starryon	365	170\r
+Pyrin	360	39\r
+Prixter	355	160\r
+Quivern	350	132\r
+Selyne	345	153\r
+Warsect	340	125\r
+Quivern Botan	340	133\r
+Elizabee	330	100\r
+Reptyro	320	53\r
+Jormuntide Ignis	315	33\r
+Jormuntide	310	32\r
+Mammorest	300	72\r
+Mammorest Cryst	290	73\r
+Relaxaurus	280	58\r
+Warsect Terra	275	126\r
+Relaxaurus Lux	270	59\r
+Knocklem	265	161\r
+Xenolord	265	168\r
+Menasting	260	144\r
+Lyleen	250	98\r
+Menasting Terra	250	145\r
+Faleris Aqua	245	190\r
+Pyrin Noct	240	40\r
+Reptyro Cryst	230	54\r
+Beakon	220	124\r
+Silvegis	215	171\r
+Lyleen Noct	210	99\r
+Grizzbolt	200	16\r
+Helzephyr	190	134\r
+Helzephyr Lux	180	135\r
+Bastigor	170	191\r
+Cryolinx Terra	160	188\r
+Astegon	150	108\r
+Orserk	140	146\r
+Cryolinx	130	44\r
+Frostallion	120	70\r
+Frostallion Noct	100	71\r
+Jetragon	90	111\r
+Paladius	80	127\r
+Necromus	70	140\r
+Shadowbeak	60	82\r
+Suzaku	50	37\r
+Suzaku Aqua	30	38\r
+Blazamut	10	78\r
+Blazamut Ryu	9	79\r
+Bellanoir	1	151\r
+Bellanoir Libero	1	152\r
+`,_=`Relaxaurus	Sparkit	Relaxaurus Lux\r
+Incineram	Maraith	Incineram Noct\r
+Mau	Pengullet	Mau Cryst\r
+Vanwyrm	Foxcicle	Vanwyrm Cryst\r
+Eikthyrdeer	Hangyu	Eikthyrdeer Terra\r
+Elphidran	Surfent	Elphidran Aqua\r
+Pyrin	Katress	Pyrin Noct\r
+Mammorest	Wumpo	Mammorest Cryst\r
+Mossanda	Grizzbolt	Mossanda Lux\r
+Dinossom	Rayhound	Dinossom Lux\r
+Jolthog	Pengullet	Jolthog Cryst\r
+Frostallion	Helzephyr	Frostallion Noct\r
+Kingpaca	Reindrix	Kingpaca Cryst\r
+Lyleen	Menasting	Lyleen Noct\r
+Leezpunk	Flambelle	Leezpunk Ignis\r
+Blazehowl	Felbat	Blazehowl Noct\r
+Robinquill	Fuddler	Robinquill Terra\r
+Broncherry	Fuack	Broncherry Aqua\r
+Surfent	Dumud	Surfent Terra\r
+Gobfin	Rooby	Gobfin Ignis\r
+Suzaku	Jormuntide	Suzaku Aqua\r
+Reptyro	Foxcicle	Reptyro Cryst\r
+Hangyu	Swee	Hangyu Cryst\r
+Mossanda	Petallia	Lyleen\r
+Vanwyrm	Anubis	Faleris\r
+Mossanda	Rayhound	Grizzbolt\r
+Grizzbolt	Relaxaurus	Orserk\r
+Kitsun	Astegon	Shadowbeak\r
+Jormuntide	Blazehowl	Jormuntide Ignis\r
+Bellanoir	Bellanoir Libero	Bellanoir\r
+Katress	Wixen	Wixen Noct\r
+Katress	Wixen	Katress Ignis\r
+Gorirat	Kikit	Gorirat Terra\r
+Chillet	Arsox	Chillet Ignis\r
+Quivern	Lullu	Quivern Botan\r
+Helzephyr	Beakon	Helzephyr Lux\r
+Bushi	Sootseer	Bushi Noct\r
+Menasting	Knocklem	Menasting Terra\r
+Foxparks	Foxcicle	Foxparks Cryst\r
+Dazzi	Omascul	Dazzi Noct\r
+Cryolinx	Dazemu	Cryolinx Terra\r
+Fenglope	Azurmane	Fenglope Lux\r
+Caprity	Tarantriss	Caprity Noct\r
+Loupmoon	Sweepa	Loupmoon Cryst\r
+Faleris	Jormuntide	Faleris Aqua\r
+Ribbuny	Bristla	Ribbuny Botan\r
+Warsect	Digtoise	Warsect Terra\r
+Kitsun	Nyafia	Kitsun Noct\r
+Frostallion	Anubis	Bastigor\r
+Paladius	Lunaris	Selyne\r
+Gildane	Eikthyrdeer Terra	Celesdir\r
+`,T={};_.split(`\r
+`).map(n=>n.split("	")).filter(n=>n.length===3).forEach(n=>{T[n[2]]={a:n[0],b:n[1]}});class J{constructor(){P(this,"pals");P(this,"palsByIndex");this.pals=H.split(`\r
+`).map(t=>t.split("	")).filter(t=>t.length===3).map(t=>{const e={name:t[0],pwr:parseInt(t[1]),index:parseInt(t[2]),combos:[]};return T[e.name]&&(e.unique=T[e.name]),e}),this.pals.sort((t,e)=>t.name.localeCompare(e.name)),this.palsByIndex={};for(let t of this.pals)this.palsByIndex[t.index]?console.error("duplicate pal with index "+t.index):this.palsByIndex[t.index]=t;for(let t of this.pals)for(let e of this.pals){const l=this.breedPals(t,e);t.combos.push({partner:e,child:l})}}getPal(t){return this.palsByIndex[t]}breedPals(t,e){if(t===e)return t;const l=Math.floor((t.pwr+e.pwr+1)/2);let i=null,s=-1;for(let c of this.pals){if(c.unique){if(c.unique.a===t.name&&c.unique.b===e.name||c.unique.b===t.name&&c.unique.a===e.name)return c;continue}const p=Math.abs(c.pwr-l);if(s===-1||p<=s){if(p===s){console.log("tie breaker!",t.name," + ",e.name," = ",c.name," | ",i.name),c.index<((i==null?void 0:i.index)||0)&&(i=c),console.log("	 "+i.name+" won");continue}i=c,s=p}}return i}findBreedOptions(t,e,l){if(t===e)return[];const i=(c,p,y)=>{let m=c.combos.map(o=>({partner:o.partner,child:o.child,chains:[]}));if(p<y){for(const o of m)o.chains=i(o.child,p+1,y);m=m.filter(o=>o.chains.length>0&&o.child!==e)}else m=m.filter(o=>o.child===e);return m=m.filter(o=>o.partner!==c),m},s=i(t,0,l);return console.log("retrieved chains ",s),s}}function $(n){return n&&n.__esModule&&Object.prototype.hasOwnProperty.call(n,"default")?n.default:n}function V(n){if(n.__esModule)return n;var t=n.default;if(typeof t=="function"){var e=function l(){return this instanceof l?Reflect.construct(t,arguments,this.constructor):t.apply(this,arguments)};e.prototype=t.prototype}else e={};return Object.defineProperty(e,"__esModule",{value:!0}),Object.keys(n).forEach(function(l){var i=Object.getOwnPropertyDescriptor(n,l);Object.defineProperty(e,l,i.get?i:{enumerable:!0,get:function(){return n[l]}})}),e}var N={exports:{}};/*!
+ * Cross-Browser Split 1.1.1
+ * Copyright 2007-2012 Steven Levithan <stevenlevithan.com>
+ * Available under the MIT License
+ * ECMAScript compliant, uniform cross-browser split method
+ */var R,O;function k(){return O||(O=1,R=function(t){var e=String.prototype.split,l=/()??/.exec("")[1]===t,i;return i=function(s,d,p){if(Object.prototype.toString.call(d)!=="[object RegExp]")return e.call(s,d,p);var y=[],m=(d.ignoreCase?"i":"")+(d.multiline?"m":"")+(d.extended?"x":"")+(d.sticky?"y":""),o=0,d=new RegExp(d.source,m+"g"),g,a,b,r;for(s+="",l||(g=new RegExp("^"+d.source+"$(?!\\s)",m)),p=p===t?-1>>>0:p>>>0;(a=d.exec(s))&&(b=a.index+a[0].length,!(b>o&&(y.push(s.slice(o,a.index)),!l&&a.length>1&&a[0].replace(g,function(){for(var u=1;u<arguments.length-2;u++)arguments[u]===t&&(a[u]=t)}),a.length>1&&a.index<s.length&&Array.prototype.push.apply(y,a.slice(1)),r=a[0].length,o=b,y.length>=p)));)d.lastIndex===a.index&&d.lastIndex++;return o===s.length?(r||!d.test(""))&&y.push(""):y.push(s.slice(o)),y.length>p?y.slice(0,p):y},i}()),R}var E,A;function Q(){if(A)return E;A=1;var n=[].indexOf;return E=function(t,e){if(n)return t.indexOf(e);for(var l=0;l<t.length;++l)if(t[l]===e)return l;return-1},E}var F,D;function X(){if(D)return F;D=1;var n=Q();F=t;function t(i){var s=i.classList;if(s)return s;var c={add:p,remove:y,contains:m,toggle:o,toString:d,length:0,item:g};return c;function p(r){var u=a();n(u,r)>-1||(u.push(r),b(u))}function y(r){var u=a(),C=n(u,r);C!==-1&&(u.splice(C,1),b(u))}function m(r){return n(a(),r)>-1}function o(r){return m(r)?(y(r),!1):(p(r),!0)}function d(){return i.className}function g(r){var u=a();return u[r]||null}function a(){var r=i.className;return e(r.split(" "),l)}function b(r){var u=r.length;i.className=r.join(" "),c.length=u;for(var C=0;C<r.length;C++)c[C]=r[C];delete r[u]}}function e(i,s){for(var c=[],p=0;p<i.length;p++)s(i[p])&&c.push(i[p]);return c}function l(i){return!!i}return F}const U={},Y=Object.freeze(Object.defineProperty({__proto__:null,default:U},Symbol.toStringTag,{value:"Module"})),Z=V(Y);var K;function tt(){if(K)return N.exports;K=1;var n=k(),t=X(),e=typeof window>"u"?Z:window,l=e.document,i=e.Text;function s(){var o=[];function d(){var g=[].slice.call(arguments),a=null;function b(r){var u;function C(h){var x=n(h,/([\.#]?[^\s#.]+)/);/^\.|#/.test(x[1])&&(a=l.createElement("div")),y(x,function(L){var z=L.substring(1,L.length);L&&(a?L[0]==="."?t(a).add(z):L[0]==="#"&&a.setAttribute("id",z):a=l.createElement(L))})}if(r!=null){if(typeof r=="string")a?a.appendChild(u=l.createTextNode(r)):C(r);else if(typeof r=="number"||typeof r=="boolean"||r instanceof Date||r instanceof RegExp)a.appendChild(u=l.createTextNode(r.toString()));else if(m(r))y(r,b);else if(p(r))a.appendChild(u=r);else if(r instanceof i)a.appendChild(u=r);else if(typeof r=="object")for(var f in r)if(typeof r[f]=="function")/^on\w+/.test(f)?function(h,x){a.addEventListener?(a.addEventListener(h.substring(2),x[h],!1),o.push(function(){a.removeEventListener(h.substring(2),x[h],!1)})):(a.attachEvent(h,x[h]),o.push(function(){a.detachEvent(h,x[h])}))}(f,r):(a[f]=r[f](),o.push(r[f](function(h){a[f]=h})));else if(f==="style")if(typeof r[f]=="string")a.style.cssText=r[f];else for(var q in r[f])(function(h,x){if(typeof x=="function")a.style.setProperty(h,x()),o.push(x(function(z){a.style.setProperty(h,z)}));else var L=r[f][h].match(/(.*)\W+!important\W*$/);L?a.style.setProperty(h,L[1],"important"):a.style.setProperty(h,r[f][h])})(q,r[f][q]);else if(f==="attrs")for(var w in r[f])a.setAttribute(w,r[f][w]);else f.substr(0,5)==="data-"?a.setAttribute(f,r[f]):a[f]=r[f];else if(typeof r=="function"){var w=r();a.appendChild(u=p(w)?w:l.createTextNode(w)),o.push(r(function(x){p(x)&&u.parentElement?(u.parentElement.replaceChild(x,u),u=x):u.textContent=x}))}}return u}for(;g.length;)b(g.shift());return a}return d.cleanup=function(){for(var g=0;g<o.length;g++)o[g]();o.length=0},d}var c=N.exports=s();c.context=s;function p(o){return o&&o.nodeName&&o.nodeType}function y(o,d){if(o.forEach)return o.forEach(d);for(var g=0;g<o.length;g++)d(o[g],g)}function m(o){return Object.prototype.toString.call(o)=="[object Array]"}return N.exports}var rt=tt();const v=$(rt),B=new J,I=document.querySelector("#app");function G(n){return v("select#"+n,B.pals.map(t=>v("option",{value:t.index},t.name)))}function M(n,t){return t.map(e=>v("div.list-row",v("div.list-item",`${n.name} + ${e.partner.name} = ${e.child.name}`),v("div.sub-list",M(e.child,e.chains))))}let S=v("div.chain-list");function et(){console.log("chains");const n=B.getPal(parseInt(document.getElementById("startPal").value)),t=B.getPal(parseInt(document.getElementById("endPal").value)),e=[];e.push(B.findBreedOptions(n,t,0),B.findBreedOptions(n,t,1)),console.log(e),S.innerHTML="",S.append(v("div",M(n,e[0]))),S.append(v("div",M(n,e[1])))}I==null||I.appendChild(v("div",v("div.inputs",v("div",v("div.label","Starting Pal"),G("startPal")),v("div",v("div.label","Resulting Pal"),G("endPal")),v("button","Calculate",{onclick:()=>et()})),S));
